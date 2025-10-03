a7010329ed2e2e8d2b4208093432fec76adde968
nova <foxtreekr@naver.com>
2025-10-03 22:16:09 +0900

    chapter14 constants 상수화를 통한 Side-bar 컴포넌트 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { AppFooter, AppHeader } from "./components/common";
+import { AppFooter, AppHeader, AppSidebar } from "./components/common";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="page">
         <AppHeader />
-        <div className="container"></div>
+        <div className="container">
+          <main className="w-full h-full min-h-[720px] flex p-6 gap-6">
+            {/* 카테고리 사이드바 */}
+            <AppSidebar />
+            {/* 토픽 콘텐츠 */}
+            <section></section>
+          </main>
+        </div>
         <AppFooter />
       </div>
     </ThemeProvider>
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,4 +1,4 @@
 // 단일 파일로 export
-export { Button } from "./button";
+export { Button, buttonVariants } from "./button";
 export { Skeleton } from "./skeleton";
 export { Separator } from "./separator";
