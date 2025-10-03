48481c87764a20f7c61d928f039a1b393afa3ef1
nova <foxtreekr@naver.com>
2025-10-03 22:46:00 +0900

    chapter15 스켈레톤 UI로 대략적인 레이아웃 구성하기

---
+++ src/components/skeleton/new-topic.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from "../ui";
+
+function SkeletonNewTopic() {
+  return (
+    <div>
+      <Skeleton className="w-full h-[210px]" />
+    </div>
+  );
+}
+
+export { SkeletonNewTopic };
